b3061c8b389b784597927f9bb7108b9e40b3ba82
Patsy Price <patsy.m.price@gmail.com>
2015-01-06 08:50:13 -0800

    Add FizzBuzz

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo is a series of short Ruby programs with minitest suites added.
+
+For example, the standard fizzbuzz algorithm was built test first
---
+++ fizzbuzz.rb
@@ -0,0 +1,33 @@
+# fizzbuzz
+# divisible by 15 then print fizzbuzz
+# divisible by 3 then print fizz
+# divisible by 5 then print buzz
+# otherwise print the number
+# standard one liner
+# (1..100).each {|n| p n%15==0?"FizzBuzz" : n%5==0?"Buzz" : n%3==0?"Fizz" : n  }
+
+#! /usr/bin/ruby
+gem 'minitest'
+require 'minitest/autorun'
+
+def fizzbuzz(n)
+  p n%15==0?"FizzBuzz" : n%5==0?"Buzz" : n%3==0?"Fizz" : n
+end
+
+describe "fizzbuzz_output" do
+  it "should return the number" do
+    fizzbuzz(87).must_equal 87
+  end
+  it "should return FizzBuzz if divisible by both 3 and 5" do
+    fizzbuzz(15).must_equal "FizzBuzz"
+  end
+  it "should return Buzz if divisible by 5" do
+     fizzbuzz(10).must_equal "Buzz"
+   end
+  it "should return Fizz if divisible by 3" do
+     fizzbuzz(9).must_equal "Fizz"
+   end
+ end
+
+# running the standard FizzBuzz with tested code
+(1..100).each {|n| fizzbuzz(n) }
